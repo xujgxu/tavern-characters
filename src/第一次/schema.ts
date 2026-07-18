489780ba6843c45acb708a9528efc61cdf957163
xujgxu <44252666+xujgxu@users.noreply.github.com>
2026-07-18 11:38:49 +0800

    feat: 统合全部19个变量至schema.ts/initvar.yaml/变量更新规则

--- src/第一次/schema.ts
+++ src/第一次/schema.ts
@@ -1,11 +1,62 @@
 export const Schema = z.object({
   世界: z.object({
     时间: z.string(),
+    地点: z.string(),
   }),
 
   主角: z.object({
     饱食度: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
     精神值: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+    心情值: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+    金钱: z.coerce.number(),
+    性欲值: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+    快感值: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
   }),
+
+  APP: z.object({
+    等级: z.coerce.number().transform(v => _.clamp(v, 1, 5)),
+    经验值: z.coerce.number(),
+    已完成任务数: z.coerce.number(),
+    当前任务: z.object({
+      类型: z.string(),
+      等级: z.coerce.number(),
+      色情维度: z.coerce.number(),
+      社会维度: z.coerce.number(),
+      内容: z.string(),
+      报酬: z.coerce.number(),
+      可选挑战: z.string(),
+      状态: z.string(),
+    }).nullable(),
+  }),
+
+  学业: z.object({
+    宏观经济学上: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+    微观经济学上: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+    金融管理上: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+    宏观经济学下: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+    微观经济学下: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+    金融管理下: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+    志愿者时长: z.coerce.number(),
+    实习时长: z.coerce.number(),
+    论文进度: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+  }),
+
+  服装: z.object({
+    穿着: z.array(z.object({
+      名称: z.string(),
+      部位标签: z.array(z.string()),
+      风格标签: z.array(z.string()),
+      颜色图案标签: z.array(z.string()),
+      状态标签: z.array(z.string()),
+      色情值: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+      描述: z.string(),
+     })),
+    整体色情值: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+  }),
+
+  联系人: z.record(z.string(), z.object({
+    好感度: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
+    身份: z.string(),
+  })),
 });
 export type Schema = z.output<typeof Schema>;
