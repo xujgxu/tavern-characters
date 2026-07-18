e97a8ec1413ecae1f9aaca008f05ffd676434390
xujgxu <44252666+xujgxu@users.noreply.github.com>
2026-07-18 13:15:00 +0800

    fix: prefault值改为initvar实际值

--- src/第一次/schema.ts
+++ src/第一次/schema.ts
@@ -1,12 +1,12 @@
 export const Schema = z.object({
   世界: z.object({
     时间: z.string(),
-    地点: z.string().prefault(''),
+    地点: z.string().prefault('西部住宅区_绿洲景苑小区'),
   }),
   主角: z.object({
     饱食度: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
     精神值: z.coerce.number().transform(v => _.clamp(v, 0, 100)),
-    金钱: z.coerce.number().prefault(0),
+    金钱: z.coerce.number().prefault(1800),
     心情值: z.coerce.number().prefault(0).transform(v => _.clamp(v, 0, 100)),
     性欲值: z.coerce.number().prefault(0).transform(v => _.clamp(v, 0, 100)),
     快感值: z.coerce.number().prefault(0).transform(v => _.clamp(v, 0, 100)),
